d1c2f1582b9cbf329e9bfb0ea819c22dd9bc28df
LowLevelVirtualMan <68070406+LowLevelVirtualMan@users.noreply.github.com>
2022-03-13 16:11:41 +0000

    Update gh-pages to output generated at 084c4fb

--- index-d90fffd15411eb53.js
+++ index-d90fffd15411eb53.js
@@ -302,7 +302,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-5c66e2dbeb471d3e_bg.wasm', import.meta.url);
+        input = new URL('index-d90fffd15411eb53_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
